7a7c35117530fe4f9dc1c2bad2fce66fcbe3bc9a
devlucascalado <devlucascalado@gmail.com>
2024-05-28 18:01:04 -0300

    atividade04

--- Sequencial/atividade04/src/main/java/org/example/Main.java
+++ Sequencial/atividade04/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        double nota1, nota2, nota3, nota4;
+
+
+        System.out.println("Informe a primeira nota: ");
+        nota1 = scanner.nextDouble();
+
+        System.out.println("Informe a segunda nota: ");
+        nota2 = scanner.nextDouble();
+
+        System.out.println("Informe a terceira nota: ");
+        nota3 = scanner.nextDouble();
+
+        System.out.println("Informe a quarta nota: ");
+        nota4 = scanner.nextDouble();
+
+        double media = nota1 + nota2 + nota3 + nota4 / 4;
+
+        System.out.println("A média do aluno é: " + media);
     }
 }
