927498bf265465e56482b4a10576873b60e58573
devlucascalado <devlucascalado@gmail.com>
2024-06-02 22:28:11 -0300

    atividade12

--- Sequencial/atividade12/src/main/java/org/example/Main.java
+++ Sequencial/atividade12/src/main/java/org/example/Main.java
@@ -7,20 +7,24 @@ package org.example;
 
 import java.util.Scanner;
 import java.util.Locale;
+import java.text.DecimalFormat;
+
+
 
 public class Main {
     public static double calcularPesoIdeal(double altura) {
-        return (72 * altura) - 58;
+        return (72.7 * altura) - 58;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        DecimalFormat df = new DecimalFormat("#.00");
 
         double altura;
 
         System.out.println("Informe a sua altura: ");
         altura = scanner.nextDouble();
 
-        System.out.println("Seu peso ideal: " + calcularPesoIdeal(altura) + "kg");
+        System.out.println("Seu peso ideal: " + df.format(calcularPesoIdeal(altura)) + "kg");
     }
 }
